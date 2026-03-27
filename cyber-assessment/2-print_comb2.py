87b2f66f0c8cd704cda4338b46e7c67bb5560a23
Ethan Bouvy <ebouvy@c1r3s3.42luxembourg.lu>
2026-03-27 17:06:55 +0100

    Fix 2-print_comb2.py

--- cyber-assessment/2-print_comb2.py
+++ cyber-assessment/2-print_comb2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+result = ""
 for i in range(100):
-    if i == 99:
-        print(str(i).zfill(2))
+    if i < 99:
+        result += str(i).zfill(2) + ", "
     else:
-        print(str(i).zfill(2), end=", ")
+        result += str(i).zfill(2)
+print(result)
