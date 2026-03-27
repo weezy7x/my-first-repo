1a21f840020cd0669f6b9cd676db474877ae15d8
Ethan Bouvy <ebouvy@c1r3s3.42luxembourg.lu>
2026-03-27 17:03:13 +0100

    Add 2-print_comb2.py

--- cyber-assessment/2-print_comb2.py
+++ cyber-assessment/2-print_comb2.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        print(f"{i}{j}", end=", " if not (i == 8 and j == 9) else "\n")
+#!/usr/bin/python3
+for i in range(100):
+    print(f"{i:02d}", end=", " if i != 99 else "\n")
