274ae25bb62e90b0c39fcced2d49d91da2f359a5
Ethan Bouvy <ebouvy@c1r3s3.42luxembourg.lu>
2026-03-27 17:05:51 +0100

    Fix 2-print_comb2.py - use zfill instead of f-string

--- cyber-assessment/2-print_comb2.py
+++ cyber-assessment/2-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    print(f"{i:02d}", end=", " if i < 99 else "\n")
+    if i == 99:
+        print(str(i).zfill(2))
+    else:
+        print(str(i).zfill(2), end=", ")
