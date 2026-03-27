c935538dc90b0be78415c55bb8c4ec0b71f3c65d
Ethan Bouvy <ebouvy@c1r3s3.42luxembourg.lu>
2026-03-27 17:08:21 +0100

    Fix 2-print_comb2.py - add newline

--- cyber-assessment/2-print_comb2.py
+++ cyber-assessment/2-print_comb2.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-result = ""
 for i in range(100):
-    if i < 99:
-        result += str(i).zfill(2) + ", "
-    else:
-        result += str(i).zfill(2)
-print(result)
+    print(str(i).zfill(2) + (", " if i < 99 else "\n"), end="")
